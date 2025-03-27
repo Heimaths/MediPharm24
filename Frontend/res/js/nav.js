ccc8159d63b1266ad26e62a812134ecdc15d927c
davidjamnig <djamnig@gmx.net>
2025-03-27 10:23:01 +0100

    update

--- Frontend/res/js/nav.js
+++ Frontend/res/js/nav.js
@@ -10,20 +10,22 @@ document.addEventListener("DOMContentLoaded", function () {
                 // User ist eingeloggt
                 if (data.is_admin) {
                     navBar.innerHTML = `
-                        
-                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/cart.html">Warenkorb</a></li>
-                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/imprint.html">Impressum</a></li>
-                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/terms.html">AGB</a></li>
+
+                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/index.html">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/products.html">Produkte bearbeiten</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/profile.html">Kunden bearbeiten</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/vouchers.html">Gutscheinverwaltung</a></li>
                             <li class="nav-item"><a class="nav-link" href="/MediPharm24/Backend/logic/logout.php">Logout</a></li>
                         
                     `;
                 } else {
                     navBar.innerHTML = `
-                        
+                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/index.html">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/products.html">Produkte</a></li>
                             <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/cart.html">Warenkorb</a></li>
-                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/imprint.html">Impressum</a></li>
-                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/terms.html">AGB</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/profile.html">Mein Konto</a></li>
                             <li class="nav-item"><a class="nav-link" href="/MediPharm24/Backend/logic/logout.php">Logout</a></li>
+   
                         
                     `;
                 }
@@ -31,9 +33,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 // Nicht eingeloggt
                 navBar.innerHTML = `
                     
+                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/index.html">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/products.html">Produkte</a></li>
                             <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/cart.html">Warenkorb</a></li>
-                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/imprint.html">Impressum</a></li>
-                            <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/terms.html">AGB</a></li>
                             <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/register.html">Registrieren</a></li>
                             <li class="nav-item"><a class="nav-link" href="/MediPharm24/frontend/sites/login.html">Login</a></li>
                         
